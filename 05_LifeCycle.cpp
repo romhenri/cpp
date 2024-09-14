cee1f7bb1e194f3f50c0c0a0db4d9ea75478cd4e
Rômulo Henri <romulohenri000@gmail.com>
2024-09-13 21:09:19 -0300

    improve: LifeCycle.cpp

--- 05_LifeCycle.cpp
+++ 05_LifeCycle.cpp
@@ -8,17 +8,16 @@ public:
   ponto2d(int xx, int yy) {
     x = xx;
     y = yy;
+    print_new();
   };
 
   ponto2d() {
     x = 0;
     y = 0;
+    print_new();
   };
 
-  ~ponto2d() {
-    std::cout << "Destruindo ponto2d";
-    std::cout << std::endl;
-  };
+  ~ponto2d() { print_del(); };
 
   void set_x(int xx) { x = xx; };
   void set_y(int yy) { y = yy; };
@@ -26,10 +25,14 @@ public:
   int get_x() { return x; };
   int get_y() { return y; };
 
-  void print_info() {
-    std::cout << "(" << x ;
-    std::cout << ", " << y << ")" << std::endl;
-  }
+  std::string get_info() {
+    return "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
+  };
+
+  void print_del() { std::cout << "DEL " << get_info() << std::endl; }
+  void print_new() { std::cout << "NEW " << get_info() << std::endl; };
+
+  void print_info() { std::cout << get_info() << std::endl; };
 };
 
 int main() {
