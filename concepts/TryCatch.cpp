44b2a872420c6c1f7d4b40b6365d73fe4a06cd24
Rômulo Henri <romulohenri000@gmail.com>
2024-11-29 23:30:28 -0300

    update: Organize Folders

--- concepts/TryCatch.cpp
+++ concepts/TryCatch.cpp
@@ -2,17 +2,21 @@
 #include <stdexcept>
 
 using std::cout;
+using std::cin;
 using std::endl;
-
 using std::exception; // Exceptions Base Class
 using std::runtime_error; // Subclass of Exception
 
 int main() {
     try {
-        int age = 15;
+        int age;
+        cout << "Enter your age: ";
+        cin >> age;
+
         if (age < 18) {
             throw runtime_error("Minimum age is 18 years!");
         }
+        
         cout << "Age allowed for entry.\n";
     } 
     catch (const exception& e) {
